57f41aaf0146262b43bb3f97ff3ea96caa5425f5
manuel <jaureguiimanuel07@gmail.com>
2026-02-16 15:32:36 -0400

    Nuevo modelo para componentes de combos

--- models/producto_combo_componentes.py
+++ models/producto_combo_componentes.py
@@ -1,11 +1,23 @@
 from odoo import fields, models
 
-class ProductComboComponent(models.Model):
+class ProductoComboComponentes(models.Model):
     _name = 'product.combo.component'
     _description = 'Componente de combo'
     
-    product_id = fields.Many2one(comodel_name='product.product')
+    combo_id = fields.Many2one(
+        comodel_name='product.product',
+        string='combo',
+        required=True,
+        ondelete='cascade'
+    )
 
-    combo_id = fields.Many2one(comodel_name='product.product', string="Combo")
+    product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Componente',
+        required='True'
+    )
 
-    quantity = fields.Float(default=1)
+    quantity = fields.Float(
+        string='Cantidad por combo', 
+        default=1
+    )
